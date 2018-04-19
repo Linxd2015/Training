52b96bf75c50fadc60560a4a9d5bc82a0659d12a
xiaodong_lin <xiaodong_lin@kingdee.com>
2018-04-19 13:14:21 +0800

    upgrade to python3

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm as Form
-from wtforms import BooleanField, StringField
-from wtforms.validators import DataRequired
-
-
-class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
